5746dd4ecdae8137bbc94aa0e2f9dc988b9f0421
jkatz01 <yehonatankatz@gmail.com>
2025-04-24 19:21:19 -0400

    replace instead of insert

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type host_data struct {
 }
 
 func main() {
-	os.Remove("sqlite-database.db") // I delete the file to avoid duplicated records. 
+	//os.Remove("sqlite-database.db") // I delete the file to avoid duplicated records. 
                                     // SQLite is a file based database.
 
 	log.Println("Creating sqlite-database.db...")
@@ -33,10 +33,18 @@ func main() {
 	//insertStudent(sqliteDatabase, "0001", "Liana Kim", "Bachelor")
 	//insertStudent(sqliteDatabase, "0002", "Glen Rangel", "Bachelor")
 	host_1 := host_data{host_name: "uhhum", last_ping: time.Now()}
+	time.Sleep(200 * time.Millisecond)
 	host_2 := host_data{host_name: "someone", last_ping: time.Now()}
+	time.Sleep(200 * time.Millisecond)
 	host_3 := host_data{host_name: "guh", last_ping: time.Now()}
+	time.Sleep(200 * time.Millisecond)
+
 	insert_host(sqliteDatabase, host_1)
 	insert_host(sqliteDatabase, host_2)
+	insert_host(sqliteDatabase, host_3)
+	insert_host(sqliteDatabase, host_3)
+	insert_host(sqliteDatabase, host_3)
+	insert_host(sqliteDatabase, host_3)
 	insert_host(sqliteDatabase, host_3)
 
 	// DISPLAY INSERTED RECORDS
@@ -44,9 +52,10 @@ func main() {
 }
 
 func create_host_table(db *sql.DB) {
-	create_host_table_SQL := `CREATE TABLE host (
+	create_host_table_SQL := `
+		CREATE TABLE IF NOT EXISTS hosts (
 		"id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,		
-		"host_name" TEXT,
+		"host_name" TEXT UNIQUE,
 		"last_ping" TEXT		
 	  );` // SQL Statement for Create Table
 
@@ -62,7 +71,11 @@ func create_host_table(db *sql.DB) {
 // We are passing db reference connection from main to our method with other parameters
 func insert_host(db *sql.DB, host host_data ) {
 	log.Println("Inserting host record ...")
-	insert_host_SQL := `INSERT INTO host(host_name, last_ping) VALUES (?, ?)`
+	// TODO: change REPLACE in query to update + insert if fails
+	// 	 because REPLACE changes the primary key id
+	insert_host_SQL := `
+		REPLACE INTO hosts(host_name, last_ping) 
+		VALUES (?, ?)`
 	statement, err := db.Prepare(insert_host_SQL) // Prepare statement. 
                                                    // This is good to avoid SQL injections
 	if err != nil {
@@ -75,7 +88,7 @@ func insert_host(db *sql.DB, host host_data ) {
 }
 
 func displayStudents(db *sql.DB) {
-	row, err := db.Query("SELECT * FROM host ORDER BY host_name")
+	row, err := db.Query("SELECT * FROM hosts ORDER BY host_name")
 	if err != nil {
 		log.Fatal(err)
 	}
